19748ca5fac2afced2897031f0a30a096726ef1c
Martin Apostolov <mapostol@progress.com>
2016-10-24 16:34:01 +0300

    Unregister for push for iOS removes only Telerik Platform registration to solve iOS 10 unregister problems.

--- everlive-push-hybrid/scripts/main.js
+++ everlive-push-hybrid/scripts/main.js
@@ -1,11 +1,11 @@
-(function (global) {
+(function(global) {
     'use strict';
 
     var app = global.app = global.app || {};
 
-    app.MainViewModel = (function () {
+    app.MainViewModel = (function() {
 
-        var _onDeviceIsRegistered = function () {
+        var _onDeviceIsRegistered = function() {
             app.hideLoading();
 
             $('#registerButton').hide();
@@ -20,16 +20,16 @@
             $('#registerButton').show();
             $('#unregisterButton').hide();
         };
-        
+
         var onAndroidPushReceived = function(args) {
             alert('Android notification received: ' + JSON.stringify(args));
         };
-        
+
         var onIosPushReceived = function(args) {
             alert('iOS notification received: ' + JSON.stringify(args));
         };
-        
-        var onWP8PushReceived = function (args) {
+
+        var onWP8PushReceived = function(args) {
             alert('Windows Phone notification received: ' + JSON.stringify(args));
         };
 
@@ -47,7 +47,7 @@
                 wp8: {
                     channelName: 'EverlivePushChannel'
                 },
-                notificationCallbackAndroid : onAndroidPushReceived,
+                notificationCallbackAndroid: onAndroidPushReceived,
                 notificationCallbackIOS: onIosPushReceived,
                 notificationCallbackWP8: onWP8PushReceived,
                 customParameters: {
@@ -60,7 +60,7 @@
             app.everlive.push.register(pushSettings)
                 .then(
                     _onDeviceIsRegistered,
-                    function (err) {
+                    function(err) {
                         app.hideLoading();
                         alert('REGISTER ERROR: ' + JSON.stringify(err));
                     }
@@ -68,16 +68,30 @@
         };
 
         var unregisterFromPush = function() {
+            var unregisterPromise;
+
             app.showLoading();
 
-            app.everlive.push.unregister()
-                .then(
-                    _onDeviceUnregistered,
-                    function(err) {
-                        app.hideLoading();
-                        alert('UNREGISTER ERROR: ' + JSON.stringify(err));
-                    }
-                );
+            if (device.platform.toLowerCase() === 'ios') {
+                // Unregisters the current device from push notifications in Telerik Backend Services only.
+                // After this call completes successfully, Backend Services will no longer send notifications to this device.
+                // Note that this does not prevent the device from receiving notifications and does not invalidate push tokens.
+
+                // This avoids using the native iOS "unregisterForRemoteNotifications" method which according to Apple's documentation,
+                // should be called in rare circumstances only, such as when a new version of the app removes support for all types of remote notifications.
+                unregisterPromise = app.everlive.push.currentDevice().unregister();
+            } else {
+                // Use push.unregister() to invalidates any push tokens and unregister from Telerik Backend Services
+                unregisterPromise = app.everlive.push.unregister();
+            }
+
+            unregisterPromise.then(
+                _onDeviceUnregistered,
+                function(err) {
+                    app.hideLoading();
+                    alert('UNREGISTER ERROR: ' + JSON.stringify(err));
+                }
+            );
         };
 
         return {
